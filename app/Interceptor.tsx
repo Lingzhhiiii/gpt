bf0c247f52543cd97f7c73a979caaa2fa7e6693a
unknown <1598034026@qq.com>
2023-10-02 11:14:46 +0800

    authfail页面

--- app/Interceptor.tsx
+++ app/Interceptor.tsx
@@ -25,7 +25,7 @@ const Interceptor = () => {
           token: token,
         };
         // client端在这里附上token
-        const response = await fetch("/api/jwt", {
+        const response = await fetch("http://tools.youren.online/jwt/", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
--- app/components/authfail.tsx
+++ app/components/authfail.tsx
@@ -1,5 +1,35 @@
-const AuthFail = () => {
-  return <h1>权限不足</h1>;
+import React from "react";
+import { Container, Typography, Button, Grid } from "@mui/material";
+import { ErrorOutline as ErrorOutlineIcon } from "@mui/icons-material";
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh", // 设置容器高度为整个屏幕高度
+  },
+} as const;
+
+const AuthFail: React.FC = () => {
+  return (
+    <Container maxWidth="sm" sx={styles.container}>
+      <Grid container spacing={2} justifyContent="center" alignItems="center">
+        <Grid item>
+          <ErrorOutlineIcon color="error" style={{ fontSize: 80 }} />
+        </Grid>
+        <Grid item>
+          <Typography variant="h4" gutterBottom>
+            权限不足
+          </Typography>
+          <Typography variant="body1" paragraph>
+            抱歉，您没有足够的权限。
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default AuthFail;
